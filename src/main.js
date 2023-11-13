56aeb4e62f2279a6661ee935bb41ef4343944018
John West <contactjohnwest@gmail.com>
2023-11-12 22:47:07 -0800

    Add project structure

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import 'ant-design-vue/dist/reset.css';
+import './assets/main.css';
+
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import Antd from 'ant-design-vue';
+import App from './App.vue';
+import router from './router';
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(Antd);
+app.use(router);
+
+app.mount('#app');
